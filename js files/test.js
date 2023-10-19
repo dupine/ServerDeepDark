3b5622e9fc1fe2f0dce5aed9c6df1ac2fd206f94
Antonio Pino <Antonio Pino>
2023-10-19 09:48:19 +0200

    dfssdf

--- js files/test.js
+++ js files/test.js
@@ -1,6 +1,10 @@
-var http = require('http');
+const WebSocket = require(`ws`);
+const ws = new WebSocket.Server({ port: 3000 });
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+ws.on("connection", (ws) => {
+  console.log("I'm server")
+  let msg = "Connection Established!. M2C!";
+  ws.send(JSON.stringify(msg));
+});
+
+
