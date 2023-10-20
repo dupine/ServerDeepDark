71b6db2efeb5646737df152205b72ccaa5775487
marsioSuS <manuelito101105@gmail.com>
2023-10-20 17:09:18 +0200

    Aggiunta i file sh per fare push e pull da mac

---
+++ pull.sh
@@ -0,0 +1,9 @@
+
+
+# Esegui il pull del repository Git
+git pull https://github.com/dupine/ServerDeepDark.git # o il tuo branch di destinazione
+
+# Fai altre operazioni se necessario
+# ...
+
+echo "Pull completato!"
