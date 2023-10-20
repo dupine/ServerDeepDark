26d4b3964dc4c7d9f28081dbc95788e09439c8ea
antonio pino <antonio.pino@iisgubbio.edu.it>
2023-10-20 16:55:13 +0200

    modificati file test e client

--- js files/test.js
+++ js files/test.js
@@ -2,9 +2,9 @@ const WebSocket = require(`ws`);
 const ws = new WebSocket.Server({ port: 3000 });
 
 ws.on("connection", (ws) => {
-  console.log("I'm server")
-  let msg = "Connection Established!. M2C!";
-  ws.send(JSON.stringify(msg));
+  console.log("Client connected")
+  ws.send('Welcome, you are connected!');
+  ws.on("message", data => {
+    console.log(`Client has sent us: ${data}`)
 });
-
-
+})
