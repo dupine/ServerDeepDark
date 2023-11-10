cf65432df3b563376951d67953b9f7d9ba917b46
Antonio Pino <Antonio Pino>
2023-11-10 13:26:11 +0100

    controlli sulle variabili e prove generali

--- js files/server/Server.js
+++ js files/server/Server.js
@@ -1,5 +1,5 @@
 // imports
-const { log } = require('console');
+//const { log } = require('console');
 const WebSocketServer = require('ws');
 
 // creazione oggetti
@@ -7,22 +7,22 @@ const wss = new WebSocketServer.Server({ port: 3000 })
 var storicoMessaggi = [];
 
 // ------------------ CONNESSIONE TRAMITE WEBSOCKET ------------------
-wss.on("connection", (ws, req) => {
-    ws.id = req.headers['sec-websocket-key'];
+wss.on("connection", (ws, richiesta) => {
+    ws.id = richiesta.headers['sec-websocket-key'];
     console.log("| Nuovo client sconosciuto connesso: "+ws.id);
     ws.send('Benvenuto! Sei connesso al server, perfavore accedi!');
     
-    // gestione messaggi
-    ws.on("message", data => {
-        gestioneRichieste(data, ws);
+    // gestione richieste in arrivo
+    ws.on("message", dati => {
+        gestioneRichieste(dati, ws);
     });
  
     // client disconnesso
-    ws.on("close", () => {    
+    ws.on("close", () => {
         console.log("| cliente: "+ws.id+", disconnesso");
     });
 
-    // errori client
+    // errore
     ws.onerror = function () {
         console.log("<ERRORE>: si è verificato unn errore")
     }
@@ -32,10 +32,8 @@ console.log("| The WebSocket server is running on port 3000");
 
 
 // ------------------ GESTIONE DELLE RICHIESTE ------------------
-function gestioneRichieste(data, ws){
-    // messaggio/matteo/12:43/incontriamoci alle 8/8.30
-    var campi = String(data).split("/");
-    // TODO: aggiungere che se un utente non si è prima loggato, non può fare niente.
+function gestioneRichieste(dati, ws){
+    var campi = String(dati).split("/");
     //data.replace(/\\/," ").replace(/\\/," ");
     //var campi = String(data).split("//");
     switch(campi[0]) {
@@ -46,7 +44,7 @@ function gestioneRichieste(data, ws){
 
         // se messaggio
         case "messaggio":
-            message(campi[1], campi[2], campi[3]);
+            messaggio(campi[1], campi[2], campi[3]);
             break;
 
         // se storico
@@ -54,6 +52,7 @@ function gestioneRichieste(data, ws){
             storico(campi[1], campi[2], ws);
             break;
 
+        // richiesta sconosciuta
         default:
             console.log("<RICHIESTA> richiesta client sconosciuta!");
             ws.send("errore");
@@ -66,16 +65,17 @@ function gestioneRichieste(data, ws){
 function login(nome, password, ws){
     let trovato = false;
     let nomeEPass = nome+","+password;
-    require("fs").readFile("accounts.csv", "utf-8", (err, data) => {
-        
+    require("fs").readFile("accounts.csv", "utf-8", (err, dati) => {
         if (err) console.log(err);
         else {
-            var fileData = data.split("\n");
-            for(let i = 0; i < fileData.length && !trovato; i++ ){
-                if(nomeEPass==fileData[i].trim()){
+            var fileDati = dati.split("\n");
+            for(let i = 0; i < fileDati.length && !trovato; i++ ){
+                // trim, usato per eliminare spazi all'inizio e fine
+                if(nomeEPass==fileDati[i].trim()){
                     console.log("| Autenticazione riconosciuta");
                     ws.nome = nome;
                     ws.autenticato = true;
+                    // invio partecipanti a tutti
                     partecipanti();
                     trovato = true;
                 }
@@ -92,13 +92,13 @@ function login(nome, password, ws){
 
 
 // ------------------ GESTIONE RICHIESTA MESSAGGIO ------------------
-function message(username, data, messaggio){
+function messaggio(username, dati, messaggio){
     console.log("<RISPOSTA> invio broadcast del messaggio di "+ username+": "+messaggio);
     wss.clients.forEach(function each(client){
-        client.send("messaggio/"+username+"/"+data+"/"+messaggio);
+        client.send("messaggio/"+username+"/"+dati+"/"+messaggio);
     })
 
-    storicoMessaggi.push([changeData(data), messaggio])
+    storicoMessaggi.push([cambiaData(dati), messaggio])
 }
 
 
@@ -120,23 +120,22 @@ function partecipanti(){
 
 //-----------------STORICO CDEI MESSAGGI---------------------------------
 function storico(inizio, fine, ws){
-    inizio = changeData(inizio);
-    fine = changeData(fine);
-    let data = "";
+    inizio = cambiaData(inizio);
+    fine = cambiaData(fine);
+    let dati = "";
     for (let i = 0; i < storicoMessaggi.length; i++) {
-        data = storicoMessaggi[i][0];
-        if(inizio<=data && fine>=data)
+        dati = storicoMessaggi[i][0];
+        if(inizio<=dati && fine>=dati)
             ws.send(storicoMessaggi[i][1]);
     }
 }
 
 
 
-function changeData(data){
-    let hour, minute;
-    [hour, minute] = data.split(":");
-    if(hour.length==1) hour = "0"+hour;
-    if(minute.length==1) minute ="0"+minute;
-    return hour+":"+minute;
-    
+function cambiaData(dati){
+    let ora, minuto;
+    [ora, minuto] = dati.split(":");
+    if(ora.length==1) ora = "0"+ora;
+    if(minuto.length==1) minuto ="0"+minuto;
+    return ora+":"+minuto;
 }
